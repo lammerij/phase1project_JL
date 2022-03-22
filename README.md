092a9ccde8dc29e3660dc1a980bdd5ddbcbfdc6b
+ <+>
2022-03-22 12:56:35 -0400

    test

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Job Tracker
 
-Job Tracker is place to keep track of your ongoing job applications. 
+## What Is It?
+
+Job Tracker is place to keep track of your ongoing job applications.
+
+## Usage
+
+Use Job Tracker to keep an organized list of jobs you are interested in and have applied to.
+
+## How To Use
+
+On the home screen, click "Add Job", then enter the "Job Title" along with a "Job Description". Once the job is added it will appear below, and once "Applied" to the job will be added to the "Job List" tab and you can see all of the jobs you've applied to in one convienent place. 
+
+
+TEST
--- javascripts/index.js
+++ javascripts/index.js
@@ -1,21 +1,22 @@
 // Getting Started //
 
 let addJob = false;
+let listJob = false;
 
 document.addEventListener("DOMContentLoaded", () => {
   getData();
   postData();
-  jobApplied();
+  jobListing();
   const addBtn = document.querySelector("#new-job-btn");
   const jobFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
-    // hide & seek with the form
     addJob = !addJob;
     if (addJob) {
       jobFormContainer.style.display = "block";
     } else {
       jobFormContainer.style.display = "none";
     }
+    
   });
 });
 
@@ -24,14 +25,17 @@ document.addEventListener("DOMContentLoaded", () => {
 function getData() {
   fetch("http://localhost:3000/jobs")
     .then((res) => res.json())
-    .then((data) => renderJobs(data));
+    .then((data) => {
+      // filter data for applied = 0
+      renderJobs(data)
+    });
 }
 
 function postData() {
   const form = document.querySelector(".add-job-form");
   // console.log(form);
   form.addEventListener("submit", (event) => {
-    // event.preventDefault();
+    event.preventDefault();
     fetch("http://localhost:3000/jobs", {
       method: "POST",
       headers: {
@@ -41,56 +45,97 @@ function postData() {
       body: JSON.stringify({
         name: event.target[0].value,
         description: event.target[1].value,
-        likes: 0,
+        applied: 0,
       }),
     });
   });
 }
 
+function patchApplied() {
+  fetch("http://localhost:3000/jobs", {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      applied: newNumberOfApplied,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => (data));
+}
+
 // DOM Content //
 
 function renderJobs(arrayOfJobs) {
-  // console.log(arrayOfJobs)
   arrayOfJobs.forEach((job) => {
-    // console.log(job);
     const collection = document.getElementById("job-collection");
     const item = document.createElement("div");
-    // console.log(item);
+    const header = document.createElement("h2");
+    const jd = document.createElement("h3");
+    const button = document.createElement("button");
     item.className = "card";
     collection.appendChild(item);
-    const header = document.createElement("h2");
-    // console.log(header)
     header.innerHTML = job.name;
-    // console.log(job.name);
     item.appendChild(header);
-    const jd = document.createElement("h3");
-    // console.log(jd);
     jd.innerHTML = job.description;
-    // console.log(job.description)
     item.appendChild(jd);
-    const button = document.createElement("button");
     button.className = "like-btn";
     button.innerText = "Applied";
     button.setAttribute("id", job.id);
     item.appendChild(button);
+    // const jobList = document.getElementById("job-list");
+    button.addEventListener("click", () => {
+      // inform back-end with PATCH, applied = 1
+      // remove clicked card from DOM
+      const collection = document.getElementById("job-collection");
+      const item = document.createElement("div");
+      const header = document.createElement("h2");
+      const jd = document.createElement("h3");
+      const button = document.createElement("button");
+      item.className = "card";
+      collection.appendChild(item);
+      header.innerHTML = job.name;
+      item.appendChild(header);
+      jd.innerHTML = job.description;
+      item.appendChild(jd);
+      button.className = "like-btn";
+      button.innerText = "Applied";
+      button.setAttribute("id", job.id);
+      item.appendChild(button);
+    });
   });
 }
 
-function jobApplied() {
-  // const applied = document.getElementsByClassName("like-btn");
-  // console.log(applied);
-  const jobsAppliedArray = [document.getElementsByClassName('like-btn')]
-  console.log(jobsAppliedArray)
-  // applied.addEventListener("click", () => {});
+// Job List Functionality //
+
+function jobListing() {
+  const listBtn = document.querySelector("#job-list-btn");
+  const jobList = document.getElementById("job-list");
+  const jobDiv = document.getElementById("job-collection");
+  // console.log(jobList);
+  listBtn.addEventListener("click", () => {
+    // wipe the page
+    // filter through jobs list for applied = 1 
+    // renderJobs(filteredJobs)
+    listJob = !listJob;
+    if (listJob) {
+      jobList.style.display = "block";
+      jobDiv.style.display = "none";
+    } else {
+      jobDiv.style.display = "block";
+      jobList.style.display = "none";
+    }
+  });
 }
-jobApplied()
 
 
-
-
-// function appliedArray(){
+// function jobApplied() {
+//   const applied = document.getElementsByClassName("like-btn");
+//   // console.log(applied);
 //   const jobsAppliedArray = [document.getElementsByClassName('like-btn')]
-// console.log(jobsAppliedArray)
-
+//   console.log(jobsAppliedArray)
+//   // applied.addEventListener("click", () => {});
 // }
-// appliedArray()
+// jobApplied()
